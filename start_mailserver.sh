e80fd16863036649d914b3f7b8e41eafb352aed5
Thilo Fromm <thilo.alexander@gmail.com>
2023-11-25 11:43:00 +0100

    start_mailserver.sh: remove explicit debug DNS server

--- start_mailserver.sh
+++ start_mailserver.sh
@@ -16,10 +16,7 @@ docker network rm mailserver-network >/dev/null 2>&1 || true
 docker network create mailserver-network
 trap 'docker network rm mailserver-network' EXIT
 
-# We use Strato's DNS server by default because some default DNS servers
-#   fragment responses, which breaks opendkim.
 docker run --rm -i -p $http:80 -p $https:443 \
-               --dns  85.215.235.63  \
                -p 25:25 -p 465:465 \
                -p 143:143 -p 993:993 \
                -p 4190:4190 \
