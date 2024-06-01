e3ae638ab4060a7678d98a88adccaea57b921fb5
Thilo Fromm <thilo.alexander@gmail.com>
2024-06-01 16:41:33 +0200

    start_mailserver.sh: force ipv4 for port binding
    Signed-off-by: Thilo Fromm <thilo.alexander@gmail.com>

--- systemd/start_mailserver.sh
+++ systemd/start_mailserver.sh
@@ -8,14 +8,18 @@ if [ -f "VERSION" ] ; then
     version=":$(cat VERSION)"
 fi
 
-exec docker run --rm -i -p $http:80 -p $https:443 \
-               -p 25:25 -p 465:465 \
-               -p 143:143 -p 993:993 \
-               -p 4190:4190 \
-               -v $(pwd)/_server_workspace_:/host --env-file settings.env \
-	       --network mailserver-network \
-               --cap-add CAP_DAC_READ_SEARCH \
-               --cap-add CAP_NET_ADMIN \
-               --cap-add CAP_NET_RAW \
-               --name mailserver \
+exec docker run --rm -i \
+                --publish 0.0.0.0:$http:80 \
+                --publish 0.0.0.0:$https:443 \
+                --publish 0.0.0.0:25:25 \
+                --publish 0.0.0.0:465:465 \
+                --publish 0.0.0.0:143:143 \
+                --publish 0.0.0.0:993:993 \
+                --publish 0.0.0.0:4190:4190 \
+                -v $(pwd)/_server_workspace_:/host --env-file settings.env \
+	        --network mailserver-network \
+                --cap-add CAP_DAC_READ_SEARCH \
+                --cap-add CAP_NET_ADMIN \
+                --cap-add CAP_NET_RAW \
+                --name mailserver \
                       "ghcr.io/t-lo/mailserver${version}"
